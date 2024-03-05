431396c7f96521d1cc7e1be90186ba086266d594
Emre Taskale <110481472+EM-T-Shells@users.noreply.github.com>
2024-03-05 16:36:48 -0500

    css file and app file updated. wrote routes in app file. began writing navbar component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,17 @@
-import { createSignal } from 'solid-js'
-import solidLogo from './assets/solid.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Router } from 'solid-app-router';
+
 
 function App() {
-  const [count, setCount] = createSignal(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-      <h1>Vite + Solid</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
-      </p>
-    </>
+    <main class="bg-slate-300/20">
+      <Router>
+        Navbar
+        <Route path="/" element={'Home'} />
+        <Route path="/about" element={'About'}/>
+        <Route path="/projects" element={'Projects'}/>
+        <Route path="/contact" element={'Contact'}/>
+      </Router>
+    </main>
   )
 }
 
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <nav class="bg-slate-300/20">
+        <header>
+            Navbar
+        </header>
+    </nav>
+  )
+};
+
+export default Navbar;
